7bcbeac976a6d85d6519c5b33d0fc04bccf3000b
Edward Quarm <equarmjnr@gmail.com>
2021-07-05 02:13:43 -0500

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,10 +6,10 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
+  firstName: "Edward",
   middleName: "",
-  lastName: "Shoaib",
-  message: " Passionate about changing the world with technology. ",
+  lastName: "Quarm",
+  message: " | Engineer | Electricity Markets | Optimization | Data Scientist | Videographer | ",
   icons: [
     {
       image: "fa-github",
